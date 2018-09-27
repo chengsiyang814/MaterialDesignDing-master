9faf09849f884d489ba8e067e67efe9a7d54635a
程龙 <2908574164@qq.com>
2018-09-27 14:50:47 +0800

    code commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# MaterialDesignDing
+此Demo用来实现MaterialDesign控件的使用,目前包含BottomSheets、DrawerLayout的代码。
+
+bottomSheets
+效果如图:
+
+![效果图](http://img.blog.csdn.net/20161012222917397)
+
+DrawerLayout
+
+![效果图](http://upload-images.jianshu.io/upload_images/3485428-f448bae6615efa4c.gif?imageMogr2/auto-orient/strip)
+
+> [个人博客] (http://www.jianshu.com/users/c5b00e3dcb94/latest_articles)
+
+> 个人网站 [歌吟有梦 itdais.com ](http:itdais.com)
+
+更多内容可以关注我的公众号：丁军伟Dev（或者搜索DingAndroid）
+
+![丁军伟Dev](http://img.blog.csdn.net/20161012205410808)
---
+++ app/src/main/java/com/itdais/materialdesignding/MainActivity.java
@@ -0,0 +1,33 @@
+package com.itdais.materialdesignding;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.itdais.materialdesignding.bottomsheet.BottomSheetActivity;
+import com.itdais.materialdesignding.drawerlayout.DrawerLayoutMainActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void doclick(View v) {
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.botton_bottomsheets:
+                intent = new Intent(this, BottomSheetActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.botton_drawerlayout:
+                intent = new Intent(this, DrawerLayoutMainActivity.class);
+                startActivity(intent);
+                break;
+            default:
+        }
+    }
+}
